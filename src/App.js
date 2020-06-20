6a26a53b5b950f7df7bad5899b30c81f9a801b7b
bmai <bmai@sfu.ca>
2020-06-20 14:48:37 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import emoji from './emoji.png'
+import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello please hire me</h1> 
+      <img src={emoji} alt="please" style={{height: 200}}/>
     </div>
   );
 }
