abee210fd713bd43829956d822fddbd180a43e88
bmai <bmai@sfu.ca>
2020-08-08 22:15:56 -0700

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,52 +1,54 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var occupation = this.props.data.occupation;
+         var description = this.props.data.description;
+         var city = this.props.data.address.city;
+         var networks = this.props.data.social.map(function (network) {
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+         })
+      }
 
-    return (
-      <header id="home">
+      return (
+         <header id="home">
 
-      <nav id="nav-wrap">
+            <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-         </ul>
+               <ul id="nav" className="nav">
+                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                  <li><a className="smoothscroll" href="#about">About</a></li>
+                  <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
+                  <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               </ul>
 
-      </nav>
+            </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">Hi! I'm Benny.</h1>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <div className="row banner">
+               <div className="banner-text">
+                  <h1 className="responsive-headline">Hi! I'm Benny.</h1>
+                  <hr />
+                  <ul className="social">
+                     {networks}
+                  </ul>
+               </div>
+            </div>
 
-      <p className="scrolldown">
-             <a className="smoothscroll" href="#about"><i className="fa fa-arrow-circle-down"></i></a>
-      </p>
+            <div >
+               <p className="scrolldown" >
+                  <a className="smoothscroll" href="#about"><i className="fa fa-chevron-down"></i></a>
+               </p>
+            </div>
 
-   </header>
-    );
-  }
+         </header>
+      );
+   }
 }
 
 export default Header;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,19 +3,28 @@ import React, { Component } from 'react';
 class Portfolio extends Component {
   render() {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects, index) {
+        var projectImage = 'images/portfolio/' + projects.image;
+        // original
+        // return <div key={projects.title} className="columns portfolio-item">
+        //  clear float after 3rd item, restart new line
+        return <div key={projects.title} className="columns portfolio-item" style={
+          index === 3 ? { clear: 'both', float: 'left', display: 'block' } : {}
+        }>
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title} target="_blank">
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5 style={{ position: 'absolute', top: '15px', left:"15px", fontSize: '30px' }}>{projects.title}</h5>
+                  <div style={{ position: 'absolute', bottom: '15px', right: '15px'}}>
+                    {
+                      projects.category.map(pc => <p style={{ textAlign: 'right', fontSize: '20px', margin: '5px' }}>{pc}</p>)
+                    }
                   </div>
                 </div>
+              </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
@@ -26,20 +35,21 @@ class Portfolio extends Component {
     return (
       <section id="portfolio">
 
-      <div className="row">
+        <div className="row">
 
-         <div className="twelve columns collapsed">
+          <div className="twelve columns collapsed ">
 
             <h1>Check Out Some of My Projects</h1>
 
             {/* change bgrid-_____ for number items per row */}
-            {/* <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf"> */}
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-quarters cf">
-                {projects}
+            {/* <div id="portfolio-wrapper" className="bgrid-halves s-bgrid-halves cf"> */}
+            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+              {/* <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-quarters cf"> */}
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
