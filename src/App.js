c1a089a10f2b5736d63298ae28b4eb0ed6bb420f
bmai <bmai@sfu.ca>
2021-02-22 22:21:02 -0800

    tracking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
@@ -35,6 +36,8 @@ class App extends Component {
 
   componentDidMount() {
     this.getResumeData();
+    const TRACKING_ID = "UA-190435445-1";
+    ReactGA.initialize(TRACKING_ID);
   }
 
   render() {
