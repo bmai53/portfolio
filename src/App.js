23e6fed841d61d36bb37646a1dddc1c4c7128759
bmai <bmai@sfu.ca>
2021-02-22 22:25:21 -0800

    tracking

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
     this.getResumeData();
     const TRACKING_ID = "UA-190435445-1";
     ReactGA.initialize(TRACKING_ID);
+    ReactGA.pageview("/homepage");
   }
 
   render() {
